88c60b369fb4a011ac24a1b964c9060df5dbf748
majid aminian <majidam66@gmail.com>
2019-02-11 21:56:38 +0100

    Rename Registration.cs to Codes/Registration.cs

---
+++ Codes/Registration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+ 
+
+namespace StudentAdministration.UIL
+{
+    public partial class RegistrationForm : Form
+    {
+        
+        public RegistrationForm()
+        {
+            InitializeComponent();
+        }
+  
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            albumclass ac = new albumclass();
+            DataTable dt = new DataTable();
+            ac.Add(txtid.Text,)
+        
+        }
+
+        
+}
